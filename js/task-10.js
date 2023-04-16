1f1e9c5d469303094987c119b85b5996c2c5150c
JuliaTarasenko00 <juliatarase17@gmail.com>
2023-04-16 12:07:26 +0200

    10

--- js/task-10.js
+++ js/task-10.js
@@ -1,45 +1,42 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-// const divControls = document.querySelector('#controls');
-// const divBoxes = document.querySelector('#boxes');
-// const btnCreate = document.querySelector('[data-create]');
-// const btnDestroy = document.querySelector('[data-destroy]');
-// function createBoxes(amounts){
-//   // const box = document.createElement("div");
-//   let size = 30;
-//  for(const amount of amounts){
-//   const box = document.createElement("div");
-//   console.log(document.createElement("div"));
-//   box.style.width = `${size}px`;
-// box.style.height = `${size}px`;
-// box.style.background = getRandomHexColor();
-// divBoxes.prepend(box);
-//  }
-// // box.style.width = `${size}px`;
-// // box.style.height = `${size}px`;
-// // box.style.background = getRandomHexColor();
-// // divBoxes.prepend(box);
-// // btnCreate.addEventListener("click", onCreBtn);
-// // btnDestroy.addEventListener("click", onDecBtn);
-// // function onCreBtn() {
-// //   if (size) {
-// //     return;
-// //   }
-// //   size += 10;
-// //   box.style.width = `${size}px`;
-// //   box.style.height = `${size}px`;
-// // }
-
-// // function onDecBtn() {
-// //   if (size === 10) {
-// //     return;
-// //   }
-// //   size -= 10;
-// //   box.style.width = `${size}px`;
-// //   box.style.height = `${size}px`;
-// // }
-// }
-// console.log(createBoxes([4,3,3,3,3,3,2]));
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const divControls = document.querySelector('#controls');
+const divBoxes = document.querySelector('#boxes');
+const btnCreate = document.querySelector('[data-create]');
+const btnDestroy = document.querySelector('[data-destroy]');
+let size = 30;
+function createBoxes(amount){
+ for(let i = 0; i < amount; i += 1){
+ const box = document.createElement("div");
+  box.style.width = `${size}px`;
+  box.style.height = `${size}px`;
+  box.style.background = getRandomHexColor();
+  size += 10;
+  divBoxes.append(box);
+  btnCreate.addEventListener('click', onClickCreate);
+  btnDestroy.addEventListener('click', onClickDestroy);
+   function onClickCreate() {
+    if(size === 500){
+        return
+    }
+   size += 10;
+   box.style.width = `${size}px`;
+   box.style.height = `${size}px`;
+  }
+  function onClickDestroy(){
+    if(size === 30){
+        return
+    }
+     size -= 10;
+ box.style.width = `${size}px`;
+ box.style.height = `${size}px`; 
+  }
+}
+}
+// function destroyBoxes() {
+//     boxesContainer.innerHTML = '';
+//   }
+console.log(createBoxes(4));
